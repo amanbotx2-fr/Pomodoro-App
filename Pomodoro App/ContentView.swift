37a8e1f8e8a18784870d344bb3bb944a58368fa2
Aman Kumar <amankumarofficial23062006@gmail.com>
2026-01-02 11:56:11 +0530

    Initial Commit

---
+++ Pomodoro App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomodoro App
+//
+//  Created by aman on 02/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodoro App/Pomodoro_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_AppApp.swift
+//  Pomodoro App
+//
+//  Created by aman on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
